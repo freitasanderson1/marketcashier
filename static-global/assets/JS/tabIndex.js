2d0867300e1b7cb2ac8fb37cd929831045768a63
Anderson Freitas <freitas.dev@proton.me>
2023-12-31 14:22:50 -0300

    Start PDV e tab

--- static-global/assets/JS/tabIndex.js
+++ static-global/assets/JS/tabIndex.js
@@ -1,14 +1,17 @@
 $('.nav-link').on('click', function(){
 
     var target = $(this).attr('href')
-
+    
     $('.nav-link, .active').removeClass('active')
 
-    $('.active, .show').removeClass('active show')
+    $('.active, .inside-tab, .show').removeClass('active show')
 
     $(this).addClass('active')
 
     $(`${target}`).addClass('active show')
+    if (target != '#pdv'){
+        $(`${target}`).parent().parent().parent().addClass('active show')
+    }
     
     // console.log(target)
 
