c73a9c79f8f54ad6de2160a64939aa6837ef6319
Anderson Freitas <freitas.dev@proton.me>
2024-02-02 13:25:56 -0300

    :fire: Add search Produto by code
    Add search Produto by code

--- controle/admin/ProdutoAdmin.py
+++ controle/admin/ProdutoAdmin.py
@@ -5,5 +5,5 @@ from controle.models import Produto
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
 
-    search_fields = ['nome','estoque','preco']
+    search_fields = ['nome','codigo','estoque','preco']
     list_display = ('id', 'nome','codigo','estoque','preco','unidadePeso','ativo')
